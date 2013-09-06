4f4113fc2d9e2b3aceaa21b38e6814b53201e7f4
Liang Bo Wang <ccwang002@gmail.com>
2013-09-06 10:29:27 +0800

    using new setup.py as default
    now move to using setuptools merely, drop all distutils imports

--- setup.py
+++ setup.py
@@ -1,11 +1,89 @@
 import sys
-import subprocess
-import os
-import re
 import warnings
-from distutils.core import setup, Command
-from distutils.command.sdist import sdist as _sdist
-from distutils.errors import DistutilsSetupError
+
+#------------------------------------------------------------------------------
+# Python Version Dection
+#------------------------------------------------------------------------------
+# arguments of 3.x specific for setuptools
+setup_kwargs = {}
+if sys.version_info >= (3,):
+    setup_kwargs['use_2to3'] = False
+
+#------------------------------------------------------------------------------
+# Setuptools
+#------------------------------------------------------------------------------
+# require setuptools >= 0.9
+import pkg_resources
+try:
+    pkg_resources.require("setuptools>=0.9")
+except pkg_resources.VersionConflict:
+    from ez_setup import use_setuptools
+    use_setuptools(version="0.9.6")
+
+from setuptools import setup
+
+# Python version check, currently supports 3.3 and up
+PYTHON_2_MSG = """\
+Require Python 3.3 and up, currently 2.x not supported.
+However, we are planned to port it back to 2.7 and we are asking for HELP!
+Please help this issue:
+    https://github.com/nextbiopy/nextbiopy/issues/1
+"""
+
+if sys.version_info < (3, 3):
+    sys.exit(PYTHON_2_MSG)
+
+
+#-----------------------------------------------------------------------------
+# Package Information
+#-----------------------------------------------------------------------------
+MAJOR = 0
+MINOR = 0
+MICRO = 0
+ISRELEASED = False
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+QUALIFIER = ''       # something like rc1, rc2
+
+AUTHOR = 'Liang Bo Wang'
+AUTHOR_EMAIL = 'b98901114@ntu.edu.tw'
+MAINTAINER = 'Liang Bo Wang'
+MAINTAINER_EMAIL = 'b98901114@ntu.edu.tw'
+URL = 'https://github.com/nextbiopy/nextbiopy'
+DESCRIPTION = 'Next bio python utility library'
+LONG_DESC = """\
+**NextBiopy** is a Python package providing basic, fast, and flexible data
+structure to store file formats widely-used in Biology.
+
+NextBiopy aims to support the following file format:
+
+  - FASTA/Q
+  - BAM/SAM (using `PySAM <https://code.google.com/p/pysam/>`__)
+"""
+LICENSE = "MIT"
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.3',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research'
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+]
+
+
+#-----------------------------------------------------------------------------
+# Version Management
+#-----------------------------------------------------------------------------
+# This section of code is adapted from the pandas project
+# (https://github.com/pydata/pandas)
+# which have been permitted for use under the BSD license.
+PTH_VERSION_PY = "nextbiopy/_version.py"
+
+FULLVERSION = VERSION
+GIT_VER_CMD = ["git", "describe", "--tags", "--always"]
 
 VERSION_PY = """\
 # This file is originally generated from Git information by running
@@ -15,122 +93,65 @@ VERSION_PY = """\
 __version__ = '{:s}'
 """
 
-VERSION_ERR_MSG = """\
-Unexpected version number: {:s}.
-Git tags should be set properly, e.g., v0.1.0
-"""
-
-VER_PATH = 'nextbiopy/_version.py'
-
-# Python version check, currently supports 3.3 and up
-PYTHON_2_MSG = """\
-require Python 3.3 and up, currently 2.x not supported.
-
-However, we have planned to port it back to 2.7 and we are asking for help!
-Please help this issue:
-    https://github.com/nextbiopy/nextbiopy/issues/1
-"""
-
-if sys.version_info < (3, 3):
-    sys.exit(PYTHON_2_MSG)
-
-def update_version_py():
-    if not os.path.exists('.git'):
-        print("This does not appear to be a Git repository.")
-        return
+if not ISRELEASED:
+    FULLVERSION += '.dev'
+    import subprocess
     try:
         p = subprocess.Popen(
-            ["git", "describe", "--tags", "--dirty", "--always"],
+            GIT_VER_CMD,
             stdout=subprocess.PIPE
         )
-    except EnvironmentError:
-        print("unable to run git, leaving _version.py alone".format(VER_PATH))
-        return
-    stdout = p.communicate()[0].decode('utf8').rstrip()
+    except OSError:
+        # msysgit compatibility
+        p = subprocess.Popen(
+            GIT_VER_CMD,
+            stdout=subprocess.PIPE
+        )
+    rev = p.communicate()[0]
     if p.returncode != 0:
-        print("unable to run git, leaving _version.py alone")
-        return
-    # we use tags like "v0.5", so strip the prefix "v"
-    if not stdout.startswith('v'):
-        raise DistutilsSetupError(VERSION_ERR_MSG.format(stdout))
-    ver = stdout[len('v'):]
+        warnings.warn("WARNING: Couldn't get git revision")
+    else:
+        if sys.version_info[0] >= 3:
+            rev = rev.decode('utf8')
+        FULLVERSION = rev.rstrip().lstrip('v')
+else:
+    FULLVERSION += QUALIFIER
+
+def write_version_py():
     # write version to _version.py
-    with open(VER_PATH, 'w') as f:
-        f.write(VERSION_PY.format(ver))
-    print("set {:s} to '{:s}'".format(VER_PATH, ver))
+    with open(PTH_VERSION_PY, 'w') as f:
+        f.write(VERSION_PY.format(FULLVERSION))
+
+write_version_py()
+
+#-----------------------------------------------------------------------------
+# Unit Test using nose
+#-----------------------------------------------------------------------------
+setup_kwargs["test_suite"] = "nose.collector"
 
 
-def get_version():
-    try:
-        f = open(VER_PATH)
-    except EnvironmentError:
-        return None
-    for line in f.readlines():
-        mo = re.match("__version__ = '([^']+)'", line)
-        if mo:
-            ver = mo.group(1)
-            if ver.endswith('-dirty'):
-                warnings.warn('Not all code changes have been committed')
-            return ver
-    return None
-
-
-class Version(Command):
-    description = "update _version.py from git repo"
-    user_options = []
-    boolean_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        update_version_py()
-        ver = get_version()
-        print("version is now", ver)
-
-
-class sdist(_sdist):
-    def run(self):
-        with warnings.catch_warnings(record=True):
-            # Cause all warnings to always be triggered.
-            warnings.simplefilter("always")
-            # regenerate version number for every release
-            update_version_py()
-            ver_pattern = "^\d+[.]\d+[.]\d+$"
-            ver = get_version()
-            if not re.match(ver_pattern, ver):
-                sys.exit("version is not clean, don't issue a release")
-            self.distribution.metadata.version = ver
-        return _sdist.run(self)
-
-
+#-----------------------------------------------------------------------------
+# Setup
+#-----------------------------------------------------------------------------
 setup(
     name='nextbiopy',
-    license='MIT',
-    version=get_version(),
-    description='Next bio python utility library',
-    author=['Liang Bo Wang', 'Wen Wei Liao'],
-    author_email=['b98901114@ntu.edu.tw', 'gattacaliao@gmail.com'],
-    maintainer=['Liang Bo Wang', 'Wen Wei Liao'],
-    maintainer_email=['b98901114@ntu.edu.tw', 'gattacaliao@gmail.com'],
-    url='https://github.com/nextbiopy/nextbiopy',
     packages=['nextbiopy'],
     package_data={
-        # if any packages contains *.txt or *.rst, include them
+        # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
     },
-    scripts=[],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Utilities',
-    ],
-    cmdclass={"version": Version, "sdist": sdist},
+    zip_safe=False,     # may contain C extension
+    # metadata for PyPI
+    license=LICENSE,
+    version=FULLVERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESC,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url=URL,
+    classifiers=CLASSIFIERS,
+    platforms='any',
+    **setup_kwargs
 )
