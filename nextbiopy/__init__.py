a3256fb485035c13d71f730fa8f8286d330d39c9
Liang Bo Wang <ccwang002@gmail.com>
2013-09-09 00:32:13 +0800

    Revert "add nb.test() by wrapping nose.main()"
    This reverts commit 1bf85138542ef2c4637df66a038928174d08b8f2.

--- nextbiopy/__init__.py
+++ nextbiopy/__init__.py
@@ -4,19 +4,3 @@
 from nextbiopy._version import __version__
 
 from nextbiopy.core import Seq
-NO_NOSE_MSG = """\
-Testing NextBiopy require Python package nose, which can be installed by
-
-    $ pip install nose
-
-Cannot import package nose, abort testing.
-"""
-
-try:
-    from nose import main as test
-except ImportError:
-    def _exit_no_nose():
-        import sys
-        sys.exit(NO_NOSE_MSG)
-    test = _exit_no_nose
-
