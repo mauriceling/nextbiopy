6852d64b322c0e10390e6b408a132e28a7e7fda5
Apua <apua.a.aa@gmail.com>
2013-10-05 18:25:21 +0800

    Refix "__next__" and modify "yield from" to syntax of Python2.x
    Close #1

--- nextbiopy/io/_fastaq.py
+++ nextbiopy/io/_fastaq.py
@@ -207,12 +207,16 @@ class Fasta:
         else:
             raise NotImplementedError('Not in read mode')
 
-    #def _next__(self):
+    def __next__(self):
+        return next(self._seq_generator)
+
     def next(self):
         return next(self._seq_generator)
 
     def _gen_seq(self):
-        yield from read_fasta(self.file_path, self.multiline)
+        for s in read_fasta(self.file_path, self.multiline):
+            yield s
+        #yield from read_fasta(self.file_path, self.multiline)
 
 class Fastq(Fasta):
     """FASTQ file representation
@@ -244,4 +248,6 @@ class Fastq(Fasta):
 
     """
     def _gen_seq(self):
-        yield from read_fastq(self.file_path, self.multiline)
+        for s in read_fastq(self.file_path, self.multiline):
+            yield s
+        #yield from read_fastq(self.file_path, self.multiline)
