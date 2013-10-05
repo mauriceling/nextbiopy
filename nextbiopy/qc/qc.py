894cee84401acdd4902edbf6d9b1c20e15cbd114
Wen-Wei Liao <wwliao@gate.sinica.edu.tw>
2013-10-06 00:52:42 +0800

    plotting under interactive mode

--- nextbiopy/qc/qc.py
+++ nextbiopy/qc/qc.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from nextbiopy.qc.parser import *
 
 def base_qual(fpath):
     seq_record = to_df(read_seq(fpath))
     qual = seq_record['qual'].apply(lambda s: pd.Series(list(map(lambda c: ord(c) - 33, s))))
-    return qual.boxplot()
+    ax = qual.boxplot(grid=False)
+    plt.draw_if_interactive()
+    return ax
